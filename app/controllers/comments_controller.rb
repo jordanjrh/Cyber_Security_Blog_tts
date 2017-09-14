217950c44812898fa13cd45a17cb0c43ec4bed8f
Jordan Herrera <jordanherrerajrh@gmail.com>
2017-09-14 10:26:48 -0700

    Fixed deployment issue

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -60,7 +60,7 @@ class CommentsController < ApplicationController
   # DELETE /comments/1.json
   def destroy
     @comment.destroy
-    
+
     respond_to do |format|
       format.html { redirect_to comments_url, notice: 'Comment was successfully deleted.' }
       format.json { head :no_content }
